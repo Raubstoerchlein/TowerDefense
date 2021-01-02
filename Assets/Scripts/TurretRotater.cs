b6a163bcd046936e5311e4c8b616d29eff1b754b
Daniel Conrad <theminehamster@gmail.com>
2021-01-02 15:32:05 +0100

    Enemy Waypoints + Turret Rotation Scripts

---
+++ Assets/Scripts/TurretRotater.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretRotater : MonoBehaviour
+{
+    public Transform target;
+    public float range = 15f;
+
+    public float rotationSpeedFactor = 10f;
+
+    public string enemyTag = "enemy";
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void UpdateTarget()
+    {
+        if (target != null && Vector3.Distance(transform.position, target.transform.position) <= range)
+        {
+            return;
+        }
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if(nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    void Update()
+    {
+        if(target == null)
+        {
+            return;
+        }
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeedFactor).eulerAngles;
+        transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform Test_Creep_Pref;
+
+    public Transform SpawnPont;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            SpawnWave();
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+    void SpawnWave()
+    {
+        Instantiate(Test_Creep_Pref, SpawnPont.position, SpawnPont.rotation);
+    }
+}
